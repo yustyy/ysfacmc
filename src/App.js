d2097c834d064c2e2fbdd9c936aa270b8b1f4ff7
yusssss <yusuf.acmaci@hotmail.com>
2024-04-10 02:33:20 +0300

    ytu logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ytu_logo from './assets/ytu_logo.png'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       </li>
       <h2>Yıldız Teknik Üniversitesi Matematik Mühendisliği</h2>
       <h2>-Yusuf Açmacı</h2>
+      <img src={ytu_logo}/>
      
     </div>
   );
